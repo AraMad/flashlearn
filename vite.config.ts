1ec3c08ff91e95b5e40cd88277fea1dad5427d6f
Oryna Starkina <arina.star2@gmail.com>
2026-02-12 20:43:39 +0200

    action fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // base: './' ensures that assets are loaded relative to the index.html,
-  // which is required for GitHub Pages project sites (username.github.io/repo/).
+  plugins: [react()],
   base: './',
   build: {
     outDir: 'dist',
