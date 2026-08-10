04db8ed0e149fc321eb0a53c016ace56450cb4de
Oryna Starkina <arina.star2@gmail.com>
2026-08-10 13:58:01 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Set explicit base path for custom domain
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
